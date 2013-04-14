381171f883209ec1e76ac68e5aebebdc607f167d
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2013-04-14 16:34:11 -0300

    Iniciado a reescrita do progama 2 para c

---
+++ C/Prog_exe_02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+
+	int base, altura;
+
+ 	printf("%s\n", "Programa para o cálculo da área de um triângulo:");
+ 	printf("%s\n", "Escreva o comprimento da base deste triângulo:");
+ 	scanf("%d", &base);
+ 	printf("%s\n", "Escreva o comprimento da altura deste triângulo:");
+ 	scanf("%d", &altura);
+
+	return 0;
+}
+ 	
+ 	writeln('A área deste triângulo é de: ', (0.5 * base * altura));
+  
+ End.
